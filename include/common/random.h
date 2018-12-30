2ac787c73b4b26858c0f636b14a42876c3567bb7
ivan.aksamentov <ivan.aksamentov@gmail.com>
2018-12-30 03:54:00 +0100

    Expose underlying generator

--- include/common/random.h
+++ include/common/random.h
@@ -6,43 +6,43 @@
 
 namespace numcpp {
 class Random {
-    std::mt19937 m_generator;
+  std::mt19937 m_generator;
 
-   public:
-    Random() {
-        std::array<int, std::mt19937::state_size> seed_data;
-        std::random_device r;
-        std::generate_n(seed_data.begin(), seed_data.size(), std::ref(r));
-        std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
-        m_generator = std::mt19937(seq);
-    }
+ public:
+  Random() {
+    std::array<int, std::mt19937::state_size> seed_data;
+    std::random_device r;
+    std::generate_n(seed_data.begin(), seed_data.size(), std::ref(r));
+    std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
+    m_generator = std::mt19937(seq);
+  }
 
-    explicit Random(int seed) : m_generator(seed) {}
+  explicit Random(int seed) : m_generator(seed) {}
 
-    Random(const Random& other) = delete;
+  template <typename T>
+  inline int uniform(T begin, T end) {
+    std::uniform_int_distribution<T> dist(begin, end);
+    return dist(m_generator);
+  }
 
-    const Random& operator=(const Random& other) = delete;
+  template <typename T>
+  inline int uniform(T end) {
+    return uniform(T(0), end);
+  }
 
-    template <typename T>
-    inline int uniform(T begin, T end) {
-        std::uniform_int_distribution<T> dist(begin, end);
-        return dist(m_generator);
-    }
+  template <typename T>
+  inline float uniformf(T begin, T end) {
+    std::uniform_real_distribution<T> dist(begin, end);
+    return dist(m_generator);
+  }
 
-    template <typename T>
-    inline int uniform(T end) {
-        return uniform(T(0), end);
-    }
+  template <typename T>
+  inline float uniformf(T end) {
+    return uniformf(T(0), end);
+  }
 
-    template <typename T>
-    inline float uniformf(T begin, T end) {
-        std::uniform_real_distribution<T> dist(begin, end);
-        return dist(m_generator);
-    }
-
-    template <typename T>
-    inline float uniformf(T end) {
-        return uniformf(T(0), end);
-    }
+  inline std::mt19937& generator() {
+    return m_generator;
+  }
 };
-}
+}  // namespace numcpp
